6c40f2cbe9394910f8579b96798ee5b63b8eded0
curran <curran.kelleher@gmail.com>
2017-01-16 20:27:35 +0530

    Make package script output pretty.

--- bin/package-downloads.sh
+++ bin/package-downloads.sh
@@ -14,7 +14,15 @@ cp data/CSVs/* ./downloads/full-database-csv/
 mkdir downloads/metadata
 cp data/*.csv downloads/metadata/
 cd downloads/
+
+echo
+echo "Creating full-database-csv.zip"
 zip -r full-database-csv.zip full-database-csv
+
+echo
+echo "Creating metadata.zip"
 zip -r metadata.zip metadata
+echo
+
 rm -rf full-database-csv
 rm -rf metadata
