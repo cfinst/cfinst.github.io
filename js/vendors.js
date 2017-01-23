fe8247ec174c1ea390e69c4599f8b01ef0490cde
Seemant Kulleen <seemantk@gmail.com>
2017-01-23 07:59:03 -0800

    Move the upstream sources into their own subdirectory. Concatenate all the upstream minified sources into one file that gets included in the html sources.  Make use of gh-pages/jekyll features to make this so.

---
+++ js/vendors.js
@@ -0,0 +1,10 @@
+---
+# Some of these, at production time, should be sourced from CDN.
+# Especially: jquery and basic bootstrap
+---
+{% include_relative vendor/topojson.min.js %}
+{% include_relative vendor/d3.min.js %}
+{% include_relative vendor/d3-legend.min.js %}
+{% include_relative vendor/d3-tip.js %}
+{% include_relative vendor/jquery.min.js %}
+{% include_relative vendor/bootstrap.min.js %}
