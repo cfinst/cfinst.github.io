06c205141d63f9da559eeb68aa9e42379cf4b775
Seemant Kulleen <seemantk@gmail.com>
2017-03-21 20:32:23 -0700

    Add a legend module as a placeholder.  Put a JSON version of the jekyll config in there for dynamic legend creation.

---
+++ js/legend.js
@@ -0,0 +1,56 @@
+---
+---
+function Legend() {
+    /*
+    ** Private variables
+    */
+    var container
+      , query
+      , dispatch
+      , data = { {% for section in site.data.sections %}
+          "{{ section[0] }}": {{ section[1].legends | jsonify }}{% unless forloop.last %},{% endunless %}
+        {% endfor %} }
+    ;
+
+    /*
+    ** Main Function Object
+    */
+    function my() {
+        console.log(data, query)
+
+    } // my()
+
+    /*
+    ** Private Helper Functions
+    */
+
+    /*
+    ** Private Utility Functions
+    */
+
+    /*
+    ** API - Getter/Setter Methods
+    */
+    my.container = function (_){
+        if(!arguments.length) return container;
+        container = _;
+        return my;
+      } // my.container()
+    ;
+    my.query = function (_){
+        query = _;
+        if(!arguments.length) return query;
+        return my;
+      } // my.container()
+    ;
+    my.connect = function (_){
+        if(!arguments.length) return dispatch;
+        dispatch = _;
+        return my;
+      } // my.connect()
+    ;
+    /*
+    ** This is ALWAYS the LAST thing returned
+    */
+    return my;
+} // Legend()
