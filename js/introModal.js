4beb491b1ca38bc64b5ae2e4d197a89eef95e408
curran <curran.kelleher@gmail.com>
2017-06-19 15:59:36 +0530

    Only show intro popup once, using localStorage. Closes #267

--- js/introModal.js
+++ js/introModal.js
@@ -1,5 +1,20 @@
+// This triggers the introduction popup
+// that shows only the first time the page is loaded.
+// This function should be invoked once, on page load.
 function triggerIntroModal(){
-  $('#intro-modal').modal({
-    remote: '/modals/intro.html'
-  });
+
+  if(!localStorage.getItem("pageSeen")){
+
+    // Show the modal, via Bootstrap"s API.
+    // See http://getbootstrap.com/javascript/#via-javascript
+    $("#intro-modal").modal({
+
+      // This option tells Bootstrap to load the content from this file,
+      // which is compiled using Jekyll based on configurable content
+      // from _modals/intro.md.
+      remote: "/modals/intro.html"
+    });
+
+    localStorage.setItem("pageSeen", true);
+  }
 }
