27a17dfb890d7039fed4f1e97e1cf40555962820
curran <curran.kelleher@gmail.com>
2017-08-25 21:19:30 +0530

    Remove extra semicolons

--- js/grid.js
+++ js/grid.js
@@ -217,7 +217,6 @@ function Grid(){
       xAxisG
         .transition(t)
           .call(axisX.scale(xScale))
-        ;
       ;
       xAxisG.selectAll(".tick line")
           .attr("transform", "translate(" + (xScale.step() / 2) + ",0)")
@@ -237,7 +236,7 @@ function Grid(){
         .selectAll(".tick")
           .attr("id", function (year){
             return "year-right-" + year;
-          });
+          })
       ;
       svg.selectAll(".y.axis .tick line")
           .attr("transform", "translate(0," + (yScale.step() / 2) + ")")
@@ -248,7 +247,7 @@ function Grid(){
               query.year = +d;
               dispatch.call("query", null, query);
             }
-          );
+          )
       ;
   } // render_axes()
 
